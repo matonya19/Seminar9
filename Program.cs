97a4adfce8d8e8a8dbc2f141dd272ac3786126b9
matonya <matonya19@mail.ru>
2023-01-14 15:50:52 +0400

    add seminar 9

---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿
+
+// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+int n = InputInt("Введите число: ");
+int m = 1;
+if (n < 1)
+{
+    Console.WriteLine("Введите положительное число");
+}
+Console.WriteLine(Number(n, m));
+
+int Number(int n, int m)
+{
+    if (n == m)
+        return n;
+    else
+        Console.Write($"{Number(n, m + 1)}, ");
+    return m;
+}
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+
+
+
+
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов 
+// в промежутке от M до N.		
+
+int InputNumber (string str)
+{
+    int number;
+    string text;
+
+    while (true)
+    {
+        Console.Write(str);
+        text = Console.ReadLine();
+        if (int.TryParse(text, out number))
+        {
+            break;
+        }
+        Console.WriteLine("Не удалось распознать число, попробуйте еще раз!");
+    }
+    return number;
+}
+
+
+int M = InputNumber ("Введите число М: ");
+int N = InputNumber ("Введите число N: ");
+
+
+int SumNumber(int m, int n)
+	{
+	    int x = m;
+		int y = n;
+		if(m > n)
+			{
+			    x = n;
+				y = m;
+			}
+		return (x + y)*(y - x + 1)/2; 
+	}
+		
+Console.WriteLine(SumNumber(M,N));
+
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+
+int m = InputInt("Введите M: ");
+int n = InputInt("Введите N: ");
+Console.WriteLine($"A({m}, {n}) = {Akkerman(m, n)}");
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    if (m > 0 && n == 0)
+        return Akkerman(m - 1, 1);
+    else
+        return Akkerman(m - 1, Akkerman(m, n - 1));
+}
